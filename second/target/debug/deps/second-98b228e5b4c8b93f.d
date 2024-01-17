e942cbbe1dea7068012df0c64904fd6000273b92
scryotic-tech <mrswagery@gmail.com>
2024-01-17 16:49:28 +0700

    Learning curve: Rust

---
+++ second/target/debug/deps/second-98b228e5b4c8b93f.d
@@ -0,0 +1,5 @@
+/Users/scryotic/Rust/second/target/debug/deps/second-98b228e5b4c8b93f: src/main.rs
+
+/Users/scryotic/Rust/second/target/debug/deps/second-98b228e5b4c8b93f.d: src/main.rs
+
+src/main.rs:
---
+++ second/target/debug/second.d
@@ -0,0 +1,1 @@
+/Users/scryotic/Rust/second/target/debug/second: /Users/scryotic/Rust/second/src/main.rs
